7ce4d91e392b182709a12a0117e59ccaa15972b4
Blightwidow <tdammaretz@lafourchette.com>
2018-10-03 14:03:57 +0200

    Rework 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import Head from '../components/Head'
+
+const Wrapper = styled.div`
+  padding: 50px 20px;
+`
+
+const Smiley = styled.p`
+  margin: 20px 10px 150px 30px;
+  font-size: 100px;
+`
 
 const NotFoundPage = () => (
-  <>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
+  <Wrapper>
+    <Head siteMetadata={{ title: 'Page not found' }}>
+      <meta name="robots" content="noindex, nofolllow" />
+    </Head>
+    <h1>The page not accessible</h1>
+    <p>The page you requested doesn't exists, or your connectivity doesn't allow you to view it.</p>
+    <Smiley>:(</Smiley>
+  </Wrapper>
 )
 
 export default NotFoundPage
