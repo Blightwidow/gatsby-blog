777e7df5bec12d7d3bd2d19fda345c3a53067462
Blightwidow <tdammaretz@lafourchette.com>
2018-10-03 14:13:29 +0200

    Switch to swap face font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,7 +17,7 @@ injectGlobal`
   @font-face {
     font-family: 'Rubik';
     font-style: normal;
-    font-display: auto;
+    font-display: swap;
     font-weight: 300;
     src: local('Rubik Light'), local('Rubik-Light'), url(https://fonts.gstatic.com/s/rubik/v7/iJWHBXyIfDnIV7Fqj2md8WA.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
@@ -26,7 +26,7 @@ injectGlobal`
   @font-face {
     font-family: 'Rubik';
     font-style: normal;
-    font-display: auto;
+    font-display: swap;
     font-weight: 400;
     src: local('Rubik'), local('Rubik-Regular'), url(https://fonts.gstatic.com/s/rubik/v7/iJWKBXyIfDnIV7nBrXw.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
@@ -35,7 +35,7 @@ injectGlobal`
   @font-face {
     font-family: 'Open Sans';
     font-style: normal;
-    font-display: auto;
+    font-display: swap;
     font-weight: 600;
     src: local('Open Sans SemiBold'), local('OpenSans-SemiBold'), url(https://fonts.gstatic.com/s/opensans/v15/mem5YaGs126MiZpBA-UNirkOUuhp.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
@@ -44,7 +44,7 @@ injectGlobal`
   @font-face {
     font-family: 'Open Sans';
     font-style: normal;
-    font-display: auto;
+    font-display: swap;
     font-weight: 400;
     src: local('Open Sans Regular'), local('OpenSans-Regular'), url(https://fonts.gstatic.com/s/opensans/v15/mem8YaGs126MiZpBA-UFVZ0b.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
