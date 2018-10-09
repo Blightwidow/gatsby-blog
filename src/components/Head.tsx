ed1654462ce35604ceab2ba17aaef06ead17af3c
Blightwidow <tdammaretz@lafourchette.com>
2018-10-09 17:33:38 +0200

    Fixing build

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,8 +1,6 @@
 import * as React from "react"
 import Helmet from "react-helmet"
 
-import { SiteMeta } from "../types/siteMeta"
-
 export interface HeadProps {
   title: string
   children?: React.ReactChild
