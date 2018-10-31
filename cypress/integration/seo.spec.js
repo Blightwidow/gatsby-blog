cc372145b5669f5bbc128e3fc3a766d01eb71041
Blightwidow <tdammaretz@lafourchette.com>
2018-10-31 12:18:18 +0100

    refactor: Stenghen tests for SEO

--- cypress/integration/seo.spec.js
+++ cypress/integration/seo.spec.js
@@ -1,6 +1,6 @@
 context("SEO", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8000')
+    cy.visit("http://localhost:8000")
   })
 
   it("Should have metas", () => {
@@ -24,13 +24,34 @@ context("SEO", () => {
   })
 
   it("Should have only one correct H1", () => {
-    cy.get('h1').should('have.length', 1)
-    cy.get('h1').contains('Theo Dammaretz')
+    cy.get("h1").should("have.length", 1)
+    cy.get("h1").contains("Theo Dammaretz")
   })
 
   it("Should have robots.txt", () => {
     cy.request("/robots.txt")
       .its("body")
       .should("be", "User-agent : *")
+  })
+
+  it("Should have no follow on external links", () => {
+    cy.get("a").each(a =>
+      cy.wrap(a).should("have.attr", "href").then(href => {
+        if (/^https?:\/\/.+/.test(href)) {
+          cy.wrap(a).should("have.attr", "rel", "nofollow noopener")
+        }
+      })
+    )
+  })
+
+  it("Should have some jsonld snippet", () => {
+    cy.get("[data-test=jsonld]")
+      .should("have.length.above", 0)
+  })
+
+  it("Should have manifest.json", () => {
+    cy.request("/manifest.json")
+      .its("body")
+      .should("to.not.be.null")
   })
 })
