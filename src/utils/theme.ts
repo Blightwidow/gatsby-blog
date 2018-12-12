f586efc5dd26623906da5a97dd45606ce0347342
Blightwidow <tdammaretz@lafourchette.com>
2018-12-12 16:50:30 +0100

    refactor(Theme): trying to clarify Theme generation

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,18 +1,41 @@
-export interface Theme {
+interface HueModifiers {
+  background: [number, number]
+  primary: [number, number]
+  primaryText: [number, number]
+  secondaryText: [number, number]
+}
+
+export class Theme implements Theme {
   background: string
   primary: string
   primaryText: string
   secondaryText: string
-}
 
-function generateTheme(hue: number, dark: boolean = false): Theme {
-  return {
-    background: `hsl(${hue}, ${dark ? "20%" : "0%"},  ${dark ? "12%" : "100%"})`,
-    primary: `hsl(${hue}, ${dark ? "100%" : "100%"},  ${dark ? "61%" : "61%"})`,
-    primaryText: `hsl(${hue}, ${dark ? "10%" : "8%"},  ${dark ? "62%" : "20%"})`,
-    secondaryText: `hsl(${hue}, ${dark ? "50%" : "8%"},  ${dark ? "72%" : "40%"})`,
+  constructor(hue: number, modifiers: HueModifiers) {
+    this.background = this.generateHSL(hue, modifiers.background)
+    this.primary = this.generateHSL(hue, modifiers.primary)
+    this.primaryText = this.generateHSL(hue, modifiers.primaryText)
+    this.secondaryText = this.generateHSL(hue, modifiers.secondaryText)
+  }
+
+  private generateHSL(hue: number, modifier: [number, number]): string {
+    return `hsl(${hue}, ${modifier[0]}%,  ${modifier[1]}%)`
   }
 }
 
-export const defaultTheme: Theme = generateTheme(346, false)
-export const darkTheme: Theme = generateTheme(275, true)
+const darkModifiers: HueModifiers = {
+  background: [20, 12],
+  primary: [100, 61],
+  primaryText: [10, 62],
+  secondaryText: [50, 72],
+}
+
+const lightModifiers: HueModifiers = {
+  background: [0, 100],
+  primary: [100, 61],
+  primaryText: [8, 20],
+  secondaryText: [8, 40],
+}
+
+export const defaultTheme: Theme = new Theme(346, lightModifiers)
+export const darkTheme: Theme = new Theme(275, darkModifiers)
