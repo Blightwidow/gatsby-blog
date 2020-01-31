f76b7c7e46f5c53b4bde14f1ae687bf6aa9a3795
Blightwidow <theo@dammaretz.fr>
2020-01-31 15:23:30 +0100

    refactor(useLite): Update useLite

--- src/utils/useLite.ts
+++ src/utils/useLite.ts
@@ -1,11 +1,11 @@
 import React from "react"
 
-export const useLite = (dependencies: string[]): boolean => {
+export const useLite = (): boolean => {
   const [isLite, setIsLite] = React.useState(true)
 
   React.useEffect(() => {
     setIsLite(!!(navigator as any).connection?.saveData || (navigator as any).connection?.effectiveType !== "4g")
-  }, dependencies)
+  }, [])
 
   return isLite
 }
